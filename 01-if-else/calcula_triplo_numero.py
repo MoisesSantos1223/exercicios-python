cec427f335083e3ea7a732cad13c6d19fc10ca43
Moisés Henrique Dos Santos Sodré <henriquedossantossodre@gmail.com>
2026-06-04 11:05:28 -0300

    Corrige nome do arquivo e melhora legibilidade do código

---
+++ 01-if-else/calcula_triplo_numero.py
@@ -0,0 +1,6 @@
+# Exercício: Calcular o triplo de um número
+
+numero = int(input("Digite um número: "))
+triplo = numero * 3
+
+print(f"O número {numero} multiplicado por 3 é {triplo}")
--- 01-if-else/calcula_triplo_numero.py.py
+++
@@ -1,4 +0,0 @@
-num = int(input("Digite um numero: "))
-Mult = num * 3
-
-print(f"O numero {num} mutiplicado por 3 deu {Mult}")
