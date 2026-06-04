b882842e9dabf72e363701ae383724a8d1c70abb
Moisés Henrique Dos Santos Sodré <henriquedossantossodre@gmail.com>
2026-06-04 11:52:51 -0300

    Add exercise to find largest and smallest numbers

---
+++ 03-lists/find_largest_and_smallest_number.py
@@ -0,0 +1,13 @@
+lista = []
+
+numero = int(input("Digite um número: "))
+numero1 = int(input("Digite o segundo número; "))
+numero2 = int(input("Digite o terceiro número: "))
+
+lista.append(numero)
+lista.append(numero1)
+lista.append(numero2)
+
+print(max(lista))
+print()
+print(min(lista))
