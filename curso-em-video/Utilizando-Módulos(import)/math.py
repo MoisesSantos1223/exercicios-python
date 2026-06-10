72bc4df5aa3e2722bd4b273643837027ec94f6ed
Moisés Henrique Dos Santos Sodré <henriquedossantossodre@gmail.com>
2026-06-09 21:51:08 -0300

    Adicionando Exemplos de math

--- curso-em-video/Utilizando-Módulos(import)/math.py
+++ curso-em-video/Utilizando-Módulos(import)/math.py
@@ -1,5 +1,16 @@
+#Primeiro exemplo
+"""
 import math
 nume = int(input("Digite um número: "))
 
 raiz = math.sqrt(nume)
 print(f"A raiz de {nume} e igual a {raiz}")
+"""
+from math import sqrt
+
+nume =int(input("Digite um número: "))
+
+raiz = sqrt(nume)
+print("raiz")
+
+# Esse é um jeito de importa só uma função, sem precisar importa todas.
