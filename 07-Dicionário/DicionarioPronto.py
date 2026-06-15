35fa05edde72c53f94c0805987ace650d0c7113a
Moisés Henrique Dos Santos Sodré <henriquedossantossodre@gmail.com>
2026-06-15 20:47:24 -0300

    Um jeito com dicionário pronto

---
+++ 07-Dicionário/DicionarioPronto.py
@@ -0,0 +1,9 @@
+produto = {
+    "nome": "arroz",
+    "preco": 25,
+    "quantidade": 7
+}
+
+print(f"Produto: {produto['nome']}")
+print(f"Preço: {produto['preco']}")
+print(f"Quantidade: {produto['quantidade']}")
