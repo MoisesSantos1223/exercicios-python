90bbafee37f11ca12bd714b0d402cf321714e996
Moisés Santos <https://github.com/MoisesSantos1223>
2026-07-11 16:47:01 -0300

    Exercicio de consseno e tangente em python

---
+++ curso-em-video/Utilizando-Módulos(import)/desafios/Desafio18.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa que leia um ângolo qualquer e mostre na tela o valor do seno,
+consseno e tangente desse ângulo.
+"""
+import math
+
+an = float(input("Digite um número"))
+seno = math.sin(math.radians(an))
+
+print(f"O ângulo de {an} tem o SENO de {seno :.2f}")
